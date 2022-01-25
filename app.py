a7c5026d0e03ed1eb5c7a2276b37ac875c8698f8
alex smith <alex.smith4640@yahoo.com>
2022-01-24 22:41:34 -0600

    added marshmallow schemas

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://qfniayojgpaioq:683084750e4874977c2bd978d8c0c23ba2d23a9e063a29bb479623beaea530ae@ec2-3-216-113-109.compute-1.amazonaws.com:5432/d8jt19tc264eed"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://qfniayojgpaioq:683084750e4874977c2bd978d8c0c23ba2d23a9e063a29bb479623beaea530ae@ec2-3-216-113-109.compute-1.amazonaws.com:5432/d8jt19tc264eed"
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
@@ -24,6 +24,20 @@ def __init__(self,name,year,days_in_month,days_in_previous_month,start_day):
     self.days_in_month = days_in_month
     self.days_in_previous_month = days_in_previous_month
     self.start_day = start_day
+
+
+class Reminder(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    day = db.Column(db.Integer, nullable=False)
+    month = db.Column(db.String, nullable=False)
+    year = db.Column(db.String, nullable=False)
+    text = db.Column(db.String,nullable=False)
+
+    def __init__(self, day, month, year, text):
+        self.day = day
+        self.month = month
+        self.year = year
+        self.text = text
 
 
 
